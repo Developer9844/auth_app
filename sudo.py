69646c32039a6190cb55848e4b6010347d079e18
Developer9844 <d3692094@gmail.com>
2025-05-08 17:39:48 +0530

    sealed secrest pplied

---
+++ sudo.py
@@ -0,0 +1,30 @@
+def find_peaks(array):
+    if not array:
+        return []
+
+    peak = array[0]
+    index = 0
+    output = []
+
+    for x in range(1, len(array)):
+        if array[x] * array[x-1] > 0:
+            if peak < 0 and array[x] < peak:
+                peak = array[x]
+                index = x
+            elif peak >= 0 and array[x] > peak:
+                peak = array[x]
+                index = x
+        else:
+            output.append((index, peak))
+            peak = array[x]
+            index = x
+
+    output.append((index, peak))
+    return output
+
+array = [1, 4, 2, -2, -9, 10, 2, 12, 2, -4, -4, -4, -4, 2, 6, 7]
+peaks = find_peaks(array)
+
+print(len(array))
+print(peaks)
+
